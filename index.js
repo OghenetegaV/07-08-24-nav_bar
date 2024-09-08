8f0d2e40f47fb676a56272c9784dd88b7a2b6bc1
Victoria <147267832+OghenetegaV@users.noreply.github.com>
2024-09-08 18:21:34 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 var hamburbugerIcon = document.getElementById("nav-icon");
 var nav = document.querySelector("#nav");
-var navContainer = document.querySelectorAll("#nav a");
 var cancelIcon = document.getElementById("cancel-icon");
 
 hamburbugerIcon.addEventListener("click", function() {
     this.style.display = "none";
     nav.style.display = "flex"; 
-    // navContainer.style.display = "inline";
     cancelIcon.style.display = "inline";
 });
 
